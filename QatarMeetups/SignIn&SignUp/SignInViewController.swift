dc5efd2b9e1bc66159ce86cb69077cb82c6520e9
Emilio Cardenas <ecarden9@stedwards.edu>
2022-11-28 21:15:25 -0600

    added app icon and sign logic

--- QatarMeetups/SignIn&SignUp/SignInViewController.swift
+++ QatarMeetups/SignIn&SignUp/SignInViewController.swift
@@ -6,24 +6,65 @@
 //
 
 import UIKit
+import CoreData
 
 class SignInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        lblErrorMessage.isHidden = true
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var lblErrorMessage: UILabel!
+    
+    @IBAction func btnSignIn(_ sender: UIButton) {
+        
+        if txtEmail.text! != "" && txtPassword.text! != "" {
+            // Retrieve data
+            do {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                let request = Account.fetchRequest() as NSFetchRequest<Account>
+
+                // Filter to not re-enter existing email
+                let pred = NSPredicate(format: "email == %@", txtEmail.text!)
+
+                request.predicate = pred
+
+                data = try context.fetch(request)
+
+                if data.count > 0 {
+                    if data[0].password == txtPassword.text! {
+                        signUpEmail = txtEmail.text!
+                        performSegue(withIdentifier: "signInToProfile", sender: self)
+                    }
+                    else {
+                        lblErrorMessage.isHidden = false
+                        lblErrorMessage.text = "Invalid Password"
+                    }
+
+
+                }
+                else {
+                    lblErrorMessage.isHidden = false
+                    lblErrorMessage.text = "Wrong email or password"
+                }
+            }
+            catch {}
+        }
+        else {
+            lblErrorMessage.text = "Please fill all fields"
+            lblErrorMessage.isHidden = false
+        }
+        
     }
-    */
 
+    
+    
 }
